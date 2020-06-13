2577affcb28ff8af360a8a242022d7a4311b69fa
Tim Rinkerman <tim.rinkerman98@gmail.com>
2020-06-13 00:57:17 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,12 @@
+#square each int of an array individually 
+#pre: an array 'say [1, 2, 3'
+#post: a mutated array '[1, 4, 9]'
+
 def square_array(array)
-  # your code here
-end
+  newArray = []
+  
+  array.length.times do |index|
+    newArray.push(array[index] ** 2)
+  end
+  newArray
+end  
