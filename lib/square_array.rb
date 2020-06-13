d6465817ad5fc2b2db4041e67e1d9d2dac42c0da
Tim Rinkerman <tim.rinkerman98@gmail.com>
2020-06-13 00:45:54 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,8 @@
+#square each int of an array individually 
+#pre: an array 'say [1, 2, 3'
+#post: a mutated array '[1, 4, 9]'
+
 def square_array(array)
-  # your code here
+  newArray = []
+  while array.length
 end
